1545b6d192ddb7a7f22987b6cde597dc91505591
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-06-29 15:23:52 -0700

    axios pokemon-api core assignment

--- react/pokemon-api/src/components/Pokemon.js
+++ react/pokemon-api/src/components/Pokemon.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Pokemon = (props) => {
     const [pokemon, setPokemon] = useState([]);
  
     useEffect(() => {
-        console.log('hello')
-        fetch('https://pokeapi.co/api/v2/pokemon')
-            .then(response => response.json())
-            .then(response => setPokemon(response.results))
-    }, []);
+        axios
+            .get('https://pokeapi.co/api/v2/pokemon')
+            .then((response) => {
+                console.log(response.data);
+                setPokemon(response.data.results);})
+            .catch((err) => console.log(err));
+            
+        }, []);
  
     return (
         <div>
-            
             {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
                 return (<li key={index}>{pokemon.name}</li>)
             })}
-            
         </div>
     );
 }
