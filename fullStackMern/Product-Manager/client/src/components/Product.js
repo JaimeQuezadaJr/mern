6d6f6632d5c6b28875a5c343ca99d4fa5403ceac
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-07-14 14:55:05 -0700

    Product Manager III Core Assignment

--- fullStackMern/Product-Manager/client/src/components/Product.js
+++ fullStackMern/Product-Manager/client/src/components/Product.js
@@ -5,6 +5,15 @@ import { Link, useParams } from 'react-router-dom';
 const Product = (props) => {
     const[product, setProduct] = useState({});
     const {id} = useParams();
+    const {removeProduct} = props;
+
+    const deleteProduct = (productId) => {
+        axios.delete(`http://localhost:8000/api/products/${productId}`)
+          .then((res) => {
+            removeProduct(productId)
+          })
+          .catch(err => console.log(err))
+      }
 
     useEffect(() => {
         axios.get(`http://localhost:8000/api/products/${id}`)
@@ -24,6 +33,7 @@ const Product = (props) => {
             <p>Price: ${product.price}</p>
             <p>Description: {product.description}</p>
             <Link to = {'/'}><button>Home</button></Link>
+            <Link to = {'/'}><button onClick={(e) => {deleteProduct(product._id)}}>Delete</button></Link>
         </div>
     </div>
   )
